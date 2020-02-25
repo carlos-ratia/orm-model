2fb9da58cf699da64de8c1f90f44e6751a52bd4b
Carlos Ratia <carlos.ratia@bunkerdb.com>
2020-02-25 17:04:29 -0300

    Add method ActiveRecordWrite::delete and ActiveRecordWrite::deleteBulk

--- tests/Traits/ModelWriterTest.php
+++ tests/Traits/ModelWriterTest.php
@@ -6,6 +6,7 @@ namespace Tests\Cratia\ORM\Model\Traits;
 
 
 use Cratia\ORM\DBAL\Interfaces\IAdapter;
+use Cratia\ORM\DQL\Filter;
 use Cratia\ORM\Model\Strategies\Read\ActiveRecordRead;
 use Cratia\ORM\Model\Strategies\Read\ActiveRecordWrite;
 use DI\DependencyException;
@@ -14,6 +15,7 @@ use Doctrine\DBAL\DBALException;
 use Exception;
 use Psr\Log\LoggerInterface;
 use Tests\Cratia\ORM\Model\EntityTest2;
+use Tests\Cratia\ORM\Model\EntityTest3;
 use Tests\Cratia\ORM\Model\Model3;
 use Tests\Cratia\ORM\Model\TestCase;
 
@@ -156,6 +158,61 @@ class ModelWriterTest extends TestCase
      * @throws NotFoundException
      * @throws Exception
      */
+    public function testUpdate2()
+    {
+        $modelCreate = new Model3();
+        $modelCreate->{'id_connection'} = 1;
+        $modelCreate->{'network_params'} = 'TEST';
+        $modelCreate->{'network_service'} = 'TEST';
+        $modelCreate->{'error_exception'} = 'TEST';
+
+        $modelCreate->setStrategyToWrite(new ActiveRecordWrite(
+            $this->getContainer()->get(IAdapter::class),
+            $this->getContainer()->get(LoggerInterface::class)
+        ));
+
+        $id = $modelCreate->create();
+
+        $this->assertIsString($id);
+
+        $modelLoad = new EntityTest2(intval($id));
+        $modelLoad->setStrategyToRead(new ActiveRecordRead(
+            $this->getContainer()->get(IAdapter::class),
+            $this->getContainer()->get(LoggerInterface::class)
+        ));
+        $modelLoad->setStrategyToWrite(new ActiveRecordWrite(
+            $this->getContainer()->get(IAdapter::class),
+            $this->getContainer()->get(LoggerInterface::class)
+        ));
+        $modelLoad->load();
+
+        $this->assertEquals($modelCreate->{'id_connection'}, $modelLoad->{'id_connection'});
+        $this->assertEquals($modelCreate->{'network_params'}, $modelLoad->{'network_params'});
+        $this->assertEquals($modelCreate->{'network_service'}, $modelLoad->{'network_service'});
+        $this->assertEquals($modelCreate->{'error_exception'}, $modelLoad->{'error_exception'});
+
+        $modelLoad->{'network_params'} = 'UPDATE';
+        $modelLoad->{'network_service'} = 'UPDATE';
+        $modelLoad->{'error_exception'} = 'UPDATE';
+
+        $result = $modelLoad->update();
+
+        $this->assertIsBool($result);
+        $this->assertTrue($result);
+
+        $modelLoad->load();
+
+        $this->assertEquals($modelCreate->{'id_connection'}, $modelLoad->{'id_connection'});
+        $this->assertEquals('UPDATE', $modelLoad->{'network_params'});
+        $this->assertEquals('UPDATE', $modelLoad->{'network_service'});
+        $this->assertEquals('UPDATE', $modelLoad->{'error_exception'});
+    }
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     * @throws Exception
+     */
     public function testDelete1()
     {
         $modelCreate = new Model3();
@@ -183,11 +240,57 @@ class ModelWriterTest extends TestCase
             $this->getContainer()->get(LoggerInterface::class)
         ));
         $modelLoad->load();
-
         $result = $modelLoad->delete();
-
         $this->assertIsBool($result);
         $this->assertTrue($result);
-
     }
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     * @throws Exception
+     */
+    public function testDelete2()
+    {
+        $modelCreate = new EntityTest3();
+        $modelCreate->inject(
+            $this->getContainer()->get(IAdapter::class),
+            $this->getContainer()->get(LoggerInterface::class)
+        );
+        $modelCreate->{'id_connection'} = 1;
+        $modelCreate->{'network_params'} = 'TEST';
+        $modelCreate->{'network_service'} = 'TEST';
+        $modelCreate->{'error_exception'} = 'TEST';
+
+        $id = $modelCreate->create();
+
+        $this->assertIsString($id);
+
+        $modelLoad = new EntityTest3(intval($id));
+        $modelLoad->inject(
+            $this->getContainer()->get(IAdapter::class),
+            $this->getContainer()->get(LoggerInterface::class)
+        );
+        $modelLoad->load();
+        $result = $modelLoad->delete();
+        $this->assertIsBool($result);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @throws DependencyException
+     * @throws NotFoundException
+     * @throws Exception
+     */
+    public function testDelete3()
+    {
+        $model = new EntityTest3();
+        $model->inject(
+            $this->getContainer()->get(IAdapter::class),
+            $this->getContainer()->get(LoggerInterface::class)
+        );
+        $result = $model->deleteBulk(Filter::eq($model->getField('disabled'), true));
+        $this->assertIsBool($result);
+    }
+
 }
